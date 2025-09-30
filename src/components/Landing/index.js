d2d4448de2556e7929c562f96e28bbbb6e10d468
haewonwon <khcw1029@daum.net>
2025-09-30 09:13:40 +0900

    Feat: PromoSection을 Landing 컴포넌트 barrel 파일에 추가 - Landing 컴포넌트들의 통합 export 관리

---
+++ src/components/Landing/index.js
@@ -0,0 +1,8 @@
+// Landing components barrel file
+export { HeroSection } from './HeroSection';
+export { RankingSection } from './RankingSection';
+export { RecommendSection } from './RecommendSection';
+export { FeatureSection } from './FeatureSection';
+export { FavoriteSection } from './FavoriteSection';
+export { PromoSection } from './PromoSection';
+export { MenuCategorySection } from './MenuCategorySection';
