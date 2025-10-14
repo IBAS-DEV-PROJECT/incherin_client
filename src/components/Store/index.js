a42feb1b5c0703b46c3a5f7f3d2cd19fe05f56b7
Kimseohyeon <seohyeon0219@naver.com>
2025-10-15 02:23:11 +0900

    Feat: 가게 목록 페이지 검색바 구현

--- src/components/Store/index.js
+++ src/components/Store/index.js
@@ -1,3 +1,4 @@
 export { StoreCard } from './StoreCard';
 export { StoreBadgeGroup } from './StoreBadgeGroup';
 export { StoreSummary } from './StoreSummary';
+export { StoreSearch } from './StoreSearch';
