c6e324148298d431477ee6bd317fcdac54de545f
junnyya <qpwoei37268@gmail.com>
2026-02-12 16:30:13 +0900

    fix: 페이지 전환 시 스크롤 최상단 이동 기능 추가

--- src/app/routes/index.jsx
+++ src/app/routes/index.jsx
@@ -2,13 +2,17 @@ import { Routes, Route } from 'react-router-dom';
 import { HomePage } from '../../pages/home';
 import { ShopListPage } from '../../pages/shop-list';
 import { ShopDetailPage } from '../../pages/shop-detail';
+import { ScrollToTop } from './ScrollToTop';
 
 export const AppRoutes = () => {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/shops" element={<ShopListPage />} />
-      <Route path="/shops/:id" element={<ShopDetailPage />} />
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/shops" element={<ShopListPage />} />
+        <Route path="/shops/:id" element={<ShopDetailPage />} />
+      </Routes>
+    </>
   );
 };
