7fd8c731e514dfcaa2dab1c224f15b26f97107cb
kimnamwoo <magictree060224@gmail.com>
2026-03-08 19:59:39 +0900

    fix: 리뷰 날짜 KST(한국 시간) 변환 오류 수정

--- src/shared/lib/date.js
+++ src/shared/lib/date.js
@@ -1,4 +1,6 @@
 export const formatDate = (date, locale = 'ko-KR') => {
   if (!date) return '';
-  return new Intl.DateTimeFormat(locale).format(new Date(date));
+  const d = new Date(date);
+  d.setHours(d.getHours() + 9); //+ 9시간(한국 표준시)
+  return new Intl.DateTimeFormat(locale).format(d);
 };
